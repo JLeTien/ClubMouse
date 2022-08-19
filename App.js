0187e0eff0360ceb45150843d3d1f73f88a8e934
JLeTien <88119490+JLeTien@users.noreply.github.com>
2022-08-19 16:32:51 +1000

    Button, image and highlight implemented

--- App.js
+++ App.js
@@ -1,30 +1,46 @@
 import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Image, 
+  Text, 
+  TouchableWithoutFeedback, 
+  TouchableOpacity,
+  SafeAreaView,
+  TouchableHighlight,
+  Button
+} from 'react-native';
 
 export default function App() {
   console.log("App executed");
-
-
+  const handlePress = () => console.log("Text pressed");
 
   return (
-    <View style={styles.container}>
-      <Text>
-        Suck my ass
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={3}>
+        Hi my name is geun, scratch my belly
       </Text>
-      <Image
-          style={{width: 150, height: 150}}
-          source={require('./assets/Geun.jpg')}
-        />
+      <TouchableHighlight onPress={() => console.log("Photo pressed")}>
+        <Image
+            //blurRadius={10}
+            style={{width: 300, height: 300}}
+            source={require('./assets/Geun.jpg')}
+          />
+      </TouchableHighlight>
+      <Button 
+        title="Click Me"
+        color="orange"
+        onPress={() => console.log("Button tapped")}> 
+      </Button>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
