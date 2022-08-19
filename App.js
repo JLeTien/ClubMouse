b2eb5e1f6741ed4bad41adab90f40dbee8f81b79
JLeTien <88119490+JLeTien@users.noreply.github.com>
2022-08-19 11:20:42 +1000

    Added project

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log("App executed");
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Hi Team Club Mouse
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+                   
