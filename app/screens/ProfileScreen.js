00cd59d574349c390787cca452f8aadf3ac8c17d
JLeTien <88119490+JLeTien@users.noreply.github.com>
2022-08-22 21:12:13 +1000

    edited profile page

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native'
+import { AppRegistry, View, Text, ImageBackground, StyleSheet, Image, SnapshotViewIOS } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from '@react-navigation/native'
@@ -10,8 +10,11 @@ const ProfileScreen = () => {
             style={styles.background}
             source={require("../assets/background.jpg")}
         >
-            <SafeAreaView>
-                <Text style={{ color: "white", fontSize: 40}}>PROFILE</Text>
+            <SafeAreaView style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/Hyoon.jpg')}/>
+                <Text style={styles.text}>Name:</Text>
+                <Text style={styles.text}>Age:</Text>
+                <Text style={styles.text}>Id:s</Text>
             </SafeAreaView>
 
         </ImageBackground>
@@ -21,9 +24,24 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+    },
+    logoContainer: {
+        justifyContent: "center",
+        height:200,
+        top:200
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        justifyContent: "flex-start"
+    },
+    text: {
+        fontSize:20,
+        color:"white"
     }
 })
 
+AppRegistry.registerComponent('IosFonts', () => IosFonts);
 export default ProfileScreen
