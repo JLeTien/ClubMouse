1eabf8c64818d093a3bb515b9c57746de681a05c
GallagherN <gallagher_8186@hotmail.com>
2022-08-27 18:45:50 +1000

    base layout for ProfileScreen, no menubar yet

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -14,13 +14,28 @@ const ProfileScreen = ({ route }) => {
             style={styles.background}
             source={require("../assets/background.jpg")}
         >
+            {/* Title */}
+            <View style={styles.profileContainer}>
+                <Text style={styles.title}>Profile</Text>
+
+            </View>
             
-            <SafeAreaView style={styles.logoContainer}>
+            {/* Avatar Logo*/}
+            <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/Hyoon.jpg')}/>
-                <Text style={styles.text}>Name:{Username}</Text>
-                <Text style={styles.text}>Age: {Age}</Text>
-                <Text style={styles.text}>Id:123123123</Text>
-            </SafeAreaView>
+            </View>
+
+            {/* User Details */}
+            <View style={styles.userContainer}>
+                <Text style={styles.userText}>Name: {Username}</Text>
+                <Text style={styles.userText}>Id: 123123123</Text>
+            </View>
+
+            {/* Level */}
+            <View style={styles.levelContainer}>
+                <Text style={styles.levelText}>Level 1</Text>
+                {/* <Image style={styles.xp} source={require('../assets/xpBar.png')}/> */}
+            </View>
         </ImageBackground>
 
         
@@ -28,24 +43,79 @@ const ProfileScreen = ({ route }) => {
 }
 
 const styles = StyleSheet.create({
+    // Deels with the background
     background: {
         flex: 1,
         justifyContent: "flex-start",
         alignItems: "center",
     },
+    // Title
+    profileContainer: {
+        // flex: 1,
+        // justifyContent: "center",
+        margin: 10,
+        top: 75,
+        // borderColor: "white",
+        // borderWidth: 1,
+        // backgroundColor: "black"
+    },
+    title: {
+        fontSize: 40,
+        color: "white",
+        margin: 10,
+        fontWeight: "bold"
+    },
+
+    // Logo
     logoContainer: {
-        justifyContent: "center",
-        height: 200,
-        top: 200
+        // flex: 0,
+        top: 75,
     },
     logo: {
         width: 200,
         height: 200,
         justifyContent: "flex-start"
     },
-    text: {
+
+    // User Details
+    userContainer: {
+        justifyContent: "center",
+        borderRadius: 40,
+        width: 300,
+        height: 75,
+        top: 95,
+        borderColor: "white",
+        borderWidth: 1,
+        backgroundColor: "#5d34a5",
+        alignItems: "center"
+    },
+    userText: {
         fontSize: 20,
         color: "white"
+
+    },
+
+    // Level
+    levelContainer: {
+        justifyContent: "center",
+        borderRadius: 40,
+        width: 300,
+        height: 50,
+        top: 105,
+        borderColor: "white",
+        borderWidth: 1,
+        backgroundColor: "#5d34a5",
+        alignItems: "center"
+    },
+    levelText: {
+        fontSize: 20,
+        color: "white",
+    },
+    xp: {
+        // width: 300,
+        // height: 50,
+        resizeMode: 4,
+        // justifyContent: "flex-start",
     }
 })
 
