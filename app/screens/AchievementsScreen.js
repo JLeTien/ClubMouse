683c052f35f9247708699c785cc12bf7e5c9483a
JLeTien <88119490+JLeTien@users.noreply.github.com>
2022-08-27 20:56:31 +1000

    new changes

---
+++ app/screens/AchievementsScreen.js
@@ -0,0 +1,27 @@
+import { AppRegistry, View, Text, ImageBackground, StyleSheet, Image, SnapshotViewIOS } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native'
+
+const AchievementsScreen = () => {
+    const navigation = useNavigation();
+    return (
+        <ImageBackground
+            style={styles.background}
+            source={require("../assets/background.jpg")}
+        >
+            <SafeAreaView style={styles.logoContainer}>
+            </SafeAreaView>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+    }
+})
+
+export default AchievementsScreen
