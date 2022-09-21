7b59613abccdf64c5f4f546ef721e66403cb2d63
GallagherN <gallagher_8186@hotmail.com>
2022-09-21 17:43:17 +1000

    Added sphere into home page and added a texture

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,35 +1,79 @@
 import React, {useState,useEffect} from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text, Button, Alert, SafeAreaView, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native'
+import {
+  Scene,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  SphereGeometry,
+} from 'three';
+import ExpoTHREE, { TextureLoader, Renderer } from 'expo-three';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { StatusBar } from 'expo-status-bar';
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-  const [time,setTime] = useState(null);
-    useEffect(() => {
-        let time = getCurrentTime();
-        setTime(time);
-      }, []);
-      const getCurrentTime = () => {
-        let today = new Date();
-        let hours = (today.getHours() < 10 ? '0' : '') + today.getHours();
-        let minutes = (today.getMinutes() < 10 ? '0' : '') + today.getMinutes();
-        let seconds = (today.getSeconds() < 10 ? '0' : '') + today.getSeconds();
-        return hours + ':' + minutes + ':' + seconds;
-      }
+
+  const onContextCreate = async ( gl /*: not sure what should be here */) => {
+    // three.js implementation
+    const scene = new Scene();
+    const camera = new PerspectiveCamera (75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+    gl.canvas = {width: gl.drawingBufferWidth, height: gl.drawingBufferHeight};
+
+    // set camera position away from sphere
+    camera.position.z = 2;
+
+    const renderer = new Renderer({ gl });
+    // set size of buffer to be equal  to drawing buffer width
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+    // image for sphere
+    const texture = new TextureLoader().load(require('../assets/Hyoon.jpg'));
+
+    // create sphere
+    // define geometry
+    const geometry = new SphereGeometry(1, 36, 36);
+    const material = new MeshBasicMaterial({ 
+      map: texture
+   });
+
+    const sphere = new Mesh(geometry, material);
+
+    // add sphere to scene
+    scene.add(sphere);
+
+    // create render function
+    const render = () => {
+      requestAnimationFrame(render);
+      // create rotate functionality
+      // rotate around x axis
+      // sphere.rotation.x += 0.01;
+
+      // rotate around y axis
+      sphere.rotation.y += 0.01
+
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+
+    // call render
+    render();
+  };
+  // const navigation = useNavigation();
   
   return (
     <SafeAreaView style={styles.background}>
       <View style={styles.top}> 
-        <Text 
-          onPress={() => navigation.navigate("HomeScreen")}
-          style={styles.text}>Good Night Anthony</Text>
+        <Text style={styles.text}>Good Night Anthony</Text>
       </View>
-      <View syle={styles.middle}>
+      <View>
+        <GLView 
+        onContextCreate={onContextCreate}
+        style={{width: 400, height: 400}}/>
+      </View>
+      {/* <View syle={styles.middle}>
         <Text style={styles.text}>{time}</Text>
-      </View>
-      <View syle={styles.bottom}>
-        <Text>Current Task</Text>
-      </View>
+      </View> */}
     </SafeAreaView>
   )
 }
@@ -45,7 +89,7 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     justifyContent: "flex-start",
     alignItems: "flex-end",
-    flex: 1,
+    flex: 0.2,
   },
   middle: {
     justifyContent: "flex-start",
