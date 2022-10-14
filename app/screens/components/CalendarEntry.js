593a285430b617f09b45226d834fefe0713364bd
unknown <bach.truong@uqconnect.edu.au>
2022-10-14 19:16:16 +1000

    fix minor error

--- app/screens/components/CalendarEntry.js
+++ app/screens/components/CalendarEntry.js
@@ -7,7 +7,6 @@ import { Card, Avatar, Button } from 'react-native-paper';
 class CalendarEntry extends Component {
     render() {
         return (
-<<<<<<< HEAD
         <TouchableOpacity style={{ marginRight: 30, marginTop: 30 }}>
         <Card>
           <Card.Content>
@@ -22,22 +21,6 @@ class CalendarEntry extends Component {
           </Card.Content>
         </Card>
       </TouchableOpacity>
-=======
-            <TouchableOpacity style={{ marginRight: 30, marginTop: 30 }}>
-                <Card>
-                    <Card.Content>
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                            }}>
-                            <Text>{item.name}</Text>
-                        </View>
-                    </Card.Content>
-                </Card>
-            </TouchableOpacity>
->>>>>>> 320401397f7328c0884d4d50ce7615395a36cb8f
         )
     }
 }
