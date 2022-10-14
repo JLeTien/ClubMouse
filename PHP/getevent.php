65d4b05a5c35ddc6d0b2c96bef0f620fb5627ec5
unknown <bach.truong@uqconnect.edu.au>
2022-10-14 19:21:00 +1000

    Update getevent.php from local repository

--- PHP/getevent.php
+++ PHP/getevent.php
@@ -4,12 +4,27 @@ $UserName = $decodedData['username'];
 
 $SQL = "SELECT * FROM events WHERE username = '$UserName'";
 $execution = mysqli_query($conn, $SQL);
+$checkEvent = mysqli_num_rows($execution);
+$dates = array();
+$times = array();
+$tasks = array();
 
-$rows = mysqli_fetch_array($execution, MYSQLI_BOTH);
-$date = $rows["date"];
-$time = $rows["time"];
-$task = $rows["task"];
+if ($checkEvent == 0) {
+	$Message = "Nothing";
+} else {
+	while($rows = mysqli_fetch_assoc($execution)) {
+	$date = $rows["date"];
+	$time = $rows["time"];
+	$task = $rows["task"];
 
-$response[] = array("Date" => $date, "Time" => $time, => "Task" => $task);
+	$dates[] = $rows["date"];
+	$times[] = $rows["time"];
+	$tasks[] = $rows["task"];
+	}
+}
+$response[] = array("Date" => $dates, "Time" => $times, "Task"=>$tasks, "Message" => $Message);
 echo json_encode($response);
+#echo json_encode($dates);
+#echo json_encode($times);
+#echo json_encode($tasks);
 ?>
